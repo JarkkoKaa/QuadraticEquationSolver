16fae11919e6e6a6bb5715addfcf66137cf5b705
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2018-07-06 22:15:31 +0300

    Add project files.

---
+++ QuadraticEquationSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuadraticEquationSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
